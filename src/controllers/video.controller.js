7fb421271d4409ce4f69b093135d05f44400c37f
md-sameer1 <md.sameer.832830@gmail.com>
2025-04-12 09:35:22 +0530

    feat: added controllers and route related to videos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -93,16 +93,237 @@ const getAllVideos = asyncHandler(async (req, res) => {
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
-  const { title, description } = req.body;       
-  // TODO: get video,
-  // TODO: upload to cloudinary,
+  const { title, description, isPublished } = req.body;
+  // TODO: title, description
+  // validate it
+  // TODO: get video and thumbnail
+  // validate it
+  // TODO: upload them to cloudinary,
   // TODO: create video
+  // upload to db
+  // send res
 
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "Title and description is required");
+  }
+
+  const thumbnailFileLocalPath = req.files?.thumbnail[0]?.path;
+  const videoFileLocalPath = req.files?.video[0]?.path;
+
+  if (!thumbnailFileLocalPath) {
+    throw new ApiError(400, "Thumbnail file is required");
+  }
+
+  if (!videoFileLocalPath) {
+    throw new ApiError(400, "Video file is required");
+  }
+
+  const thumbnail = await uploadOnCloudinary(thumbnailFileLocalPath);
+
+  if (!thumbnail?.url) {
+    throw new ApiError(
+      500,
+      "Somenthing went wrong while uploding thumbnail to cloudinary"
+    );
+  }
+
+  const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+
+  if (!videoFile?.url) {
+    throw new ApiError(
+      500,
+      "Somenthing went wrong while uploding video to cloudinary"
+    );
+  }
+
+  const video = await Video.create({
+    title,
+    description,
+    thumbnail: thumbnail?.url,
+    videoFile: videoFile?.url,
+    isPublished,
+    duration: videoFile?.duration,
+    owner: req?.user?._id,
+  });
+
+  if (!video) {
+    throw new ApiError(500, "Something went wrong while creating video");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, video, "Video created successfully"));
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: get video by id
+  // get video by id
+  // validation of video id
+  // aggrigate video data from mongodb using checks like $match : _id and $match : isPublished
+  // aggrigation of likes
+  // aggrigation of comments -> its users
+  // aggrigation of video owner and it's subscription info
+
+  if (!videoId) {
+    throw new ApiError(404, "Video id not found");
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Video id is not valid");
+  }
+
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $match: {
+        isPublished: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers",
+              },
+              isSubscribed: {
+                $cond: {
+                  if: {
+                    $in: [req.user?._id, "$subscribers.subscriber"],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              fullName: 1,
+              avatar: 1,
+              subscribersCount: 1,
+              isSubscribed: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            if: {
+              $in: [req?.user?._id, "$likes.likedBy"],
+              then: true,
+              else: false,
+            },
+          },
+        },
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        likesCount: 1,
+        isLiked: 1,
+        views: 1,
+        owner: 1,
+        createdAt: 1,
+        duration: 1,
+        comments: 1,
+        isPublished: 1,
+      },
+    },
+  ]);
+
+  console.log("Video by id", video);
+
+  await Video.findByIdAndUpdate(videoId, {
+    $in: {
+      views: 1,
+    },
+  });
+
+  await User.findByIdAndUpdate(req.user?._id, {
+    $addToSet: {
+      watchHistory: videoId,
+    },
+  });
+
+  if (!video?.length > 0) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetched successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getAllVideos } from "../controllers/video.controller.js";
+import {
+  getAllVideos,
+  publishAVideo,
+} from "../controllers/video.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/get-all-Videos").get(getAllVideos);
+
+router.route("/publish-video").post(publishAVideo);
+
+router.route("/get-video/:videoId");
 
 export default router;
